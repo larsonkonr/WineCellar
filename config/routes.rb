566bd30566c7f1a16932a0a1375745d3f8af39e6
Konr Larson <larsonkonr@gmail.com>
2015-03-04 15:52:26 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 root 'welcome#index'
 resources 'cellar'
-get '/auth/:provider/callback' => "sessions#create"
+get '/auth/twitter/callback' => "sessions#create"
 get "/signout" => "sessions#destroy", :as => :signout
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
