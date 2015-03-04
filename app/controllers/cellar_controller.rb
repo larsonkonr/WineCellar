ee9e8a71ff8ecf14fdf9336ff970368f43bcf87c
Konr Larson <larsonkonr@gmail.com>
2015-03-04 09:23:51 -0700

    WIP

---
+++ app/controllers/cellar_controller.rb
@@ -0,0 +1,2 @@
+class CellarController < ApplicationController
+end
---
+++ test/controllers/cellar_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CellarControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
