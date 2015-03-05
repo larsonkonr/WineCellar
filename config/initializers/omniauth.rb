22170b193ccb1bf56fe02d6b223162a13a64eddd
Konr Larson <larsonkonr@gmail.com>
2015-03-04 21:11:36 -0700

    added figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, 'vfuCG49b2ht7jpzAT62V9fwMc', '1lEDabyEad89saVM7XXUvt6FuMm1rLw1XuwYKS4Oty2CkUUIIg'
+    provider :twitter, ENV["twitter_key"], ENV["twitter_secret"]
 end
